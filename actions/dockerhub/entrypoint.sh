7f4939d9b8e9b47b21b0ba62ff91e3be623fc9c9
afragapane <ahfragapane@gmail.com>
2019-08-28 10:50:50 -0700

    sid

--- actions/dockerhub/entrypoint.sh
+++ actions/dockerhub/entrypoint.sh
@@ -14,7 +14,7 @@ echo GITHUB_REF: $GITHUB_REF
 echo ref_tmp: $ref_tmp
 
 image_tag_tmp=${ref_value//\//-} ## replace `/` with `-` in ref for docker tag requirement (master or 2019-03-13)
-IMAGE_TAG = ${image_tag_tmp//@/-}
+IMAGE_TAG = ${image_tag_tmp//\@/-}
 NAMESPACE=${DOCKER_NAMESPACE:-$USERNAME} ## use github username as docker namespace unless specified
 IMAGE_NAME=${DOCKER_IMAGE_NAME:-$REPOSITORY} ## use github repository name as docker image name unless specified
 REGISTRY_IMAGE="$DOCKER_NAMESPACE/$IMAGE_NAME"
