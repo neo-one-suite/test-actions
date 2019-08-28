e5cfbb5e8c69252372e6dea84039c902516d2a73
afragapane <ahfragapane@gmail.com>
2019-08-28 11:00:39 -0700

    nsdads

--- actions/dockerhub/entrypoint.sh
+++ actions/dockerhub/entrypoint.sh
@@ -14,7 +14,7 @@ echo GITHUB_REF: $GITHUB_REF
 echo ref_tmp: $ref_tmp
 
 image_tag_tmp=${ref_value//\//-} ## replace `/` with `-` in ref for docker tag requirement (master or 2019-03-13)
-# IMAGE_TAG = ${image_tag_tmp//\@/-}
+IMAGE_TAG = ${image_tag_tmp//\@/-}
 NAMESPACE=${DOCKER_NAMESPACE:-$USERNAME} ## use github username as docker namespace unless specified
 IMAGE_NAME=${DOCKER_IMAGE_NAME:-$REPOSITORY} ## use github repository name as docker image name unless specified
 REGISTRY_IMAGE="$DOCKER_NAMESPACE/$IMAGE_NAME"
@@ -38,6 +38,6 @@ docker push $REGISTRY_IMAGE:latest
 
 if [ "${ref_type}" = "tags" ]
 then
-  docker tag $IMAGE_NAME $REGISTRY_IMAGE:$image_tag_tmp
-  docker push $REGISTRY_IMAGE:$image_tag_tmp
+  docker tag $IMAGE_NAME $REGISTRY_IMAGE:$IMAGE_TAG
+  docker push $REGISTRY_IMAGE:$IMAGE_TAG
 fi
