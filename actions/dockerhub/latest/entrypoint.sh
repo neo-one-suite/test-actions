cd34293eed444a443896b7154a582c6c2c567477
afragapane <ahfragapane@gmail.com>
2019-09-17 16:24:18 -0700

    test

--- actions/dockerhub/latest/entrypoint.sh
+++ actions/dockerhub/latest/entrypoint.sh
@@ -9,7 +9,6 @@ REPOSITORY=${GITHUB_REPOSITORY#*/}
 
 SHA_TAG=${GITHUB_SHA:0:7}
 LATEST_TAG=latest
-IMAGE_NAME=${DOCKER_IMAGE_NAME:-$REPOSITORY} ## use github repository name as docker image name unless specified
 REGISTRY_IMAGE="$DOCKER_NAMESPACE/$IMAGE_NAME"
 echo IMAGE_NAME: $IMAGE_NAME
 echo REGISTRY_IMAGE: $REGISTRY_IMAGE
@@ -21,12 +20,12 @@ then
 fi
 
 ## build the image locally
-docker build -t $IMAGE_NAME ${*:-.} ## pass in the build command from user input, otherwise build in default mode
+docker build -t $DOCKER_IMAGE_NAME ${*:-.} ## pass in the build command from user input, otherwise build in default mode
 
 # push sha tagged image to the repository
-docker tag $IMAGE_NAME $REGISTRY_IMAGE:$SHA_TAG
+docker tag $DOCKER_IMAGE_NAME $REGISTRY_IMAGE:$SHA_TAG
 docker push $REGISTRY_IMAGE:$SHA_TAG
 
 # push latest tagged image to the repository
-docker tag $IMAGE_NAME $REGISTRY_IMAGE:$LATEST_TAG
+docker tag $DOCKER_IMAGE_NAME $REGISTRY_IMAGE:$LATEST_TAG
 docker push $REGISTRY_IMAGE:$LATEST_TAG
