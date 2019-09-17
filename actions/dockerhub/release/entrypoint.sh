fedc867e1fbca78cea36d6ee09c028fc922767cf
afragapane <ahfragapane@gmail.com>
2019-09-17 16:02:42 -0700

    fix

--- actions/dockerhub/release/entrypoint.sh
+++ actions/dockerhub/release/entrypoint.sh
@@ -7,6 +7,14 @@ set -u -e -o pipefail
 USERNAME=${GITHUB_REPOSITORY%%/*}
 REPOSITORY=${GITHUB_REPOSITORY#*/}
 
+ref_tmp=${GITHUB_REF#*/} ## throw away the first part of the ref (GITHUB_REF=refs/heads/master or refs/tags/2019/03/13)
+ref_type=${ref_tmp%%/*} ## extract the second element of the ref (heads or tags)
+ref_value=${ref_tmp#*/} ## extract the third+ elements of the ref (master or 2019/03/13)
+echo GITHUB_REF: $GITHUB_REF
+echo ref_tmp: $ref_tmp
+echo ref_type: $ref_type
+echo ref_value: $ref_value
+
 LATEST_TAG=latest
 NAMESPACE=${DOCKER_NAMESPACE:-$USERNAME} ## use github username as docker namespace unless specified
 IMAGE_NAME=${DOCKER_IMAGE_NAME:-$REPOSITORY} ## use github repository name as docker image name unless specified
@@ -25,8 +33,8 @@ fi
 docker build -t $IMAGE_NAME ${*:-.} ## pass in the build command from user input, otherwise build in default mode
 
 # push sha tagged image to the repository
-docker tag $IMAGE_NAME $REGISTRY_IMAGE:$GITHUB_REF
-docker push $REGISTRY_IMAGE:$GITHUB_REF
+docker tag $IMAGE_NAME $REGISTRY_IMAGE:$ref_value
+docker push $REGISTRY_IMAGE:$ref_value
 
 # push latest tagged image to the repository
 docker tag $IMAGE_NAME $REGISTRY_IMAGE:$LATEST_TAG
